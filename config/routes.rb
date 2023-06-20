67d63b545278f03a7d11fee0d7580fa86dd3aa10
日下部伸吾 <kusakabeshingo@kusakabeshingonoMacBook-Pro.local>
2023-06-20 21:06:33 +0900

    打開策を知りたい

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
-Rails.application.routes.draw doz
-  devise_for :users, :controllers => {
-  :registrations => 'users/registrations',
-  :sessions => 'users/sessions'   
-} 
-
-devise_scope :user do
-  get "user/:id", :to => "users/registrations#detail"
-  get "signup", :to => "users/registrations#new"
-  get "login", :to => "users/sessions#new"
-  get "logout", :to => "users/sessions#destroy"
-end
-
+Rails.application.routes.draw do
+  root to:"tops#index"
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
